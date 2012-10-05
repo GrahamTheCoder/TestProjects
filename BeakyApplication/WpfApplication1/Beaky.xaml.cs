62a4d24ef552e1b833cb786caa68eae2dcbb12a9
GrahamTheCoder <GrahamTheCoder@gmail.com>
2012-10-05 01:31:39 +0100

    Add Beaky inheriting from Popup

---
+++ BeakyApplication/WpfApplication1/Beaky.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for Beaky.xaml
+    /// </summary>
+    public partial class Beaky 
+    {
+        public Beaky()
+        {
+            InitializeComponent();
+        }
+
+        public void TieToControl(IActivatable parent)
+        {
+            parent.StateChanged += (_, __) => IsOpen = parent.WindowState != WindowState.Minimized;
+            parent.Activated += (_, __) => IsOpen = true;
+            parent.Deactivated += (_, __) => IsOpen = false;
+        }
+
+    }
+
+}
---
+++ BeakyApplication/WpfApplication1/VsWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for VsWindow.xaml
+    /// </summary>
+    public partial class VsWindow : Window, IActivatable
+    {
+        public VsWindow()
+        {
+            InitializeComponent();
+            m_Beaky.TieToControl(this);
+        }
+
+  
+        private void bob3_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+
+
+        
+    }
+
+    public interface IActivatable
+    {
+        WindowState WindowState { get; set; }
+        event EventHandler StateChanged;
+        event EventHandler Activated;
+        event EventHandler Deactivated;
+        event RoutedEventHandler Loaded;
+        event RoutedEventHandler Unloaded;
+    }
+}
